3e82074139852318897bef34376f7503c70a8d2e
Jerahmie Radder <radd0012@umn.edu>
2021-02-14 10:18:42 -0600

    added gpu4dl* enviroment for pytorch mnist

--- pytorch_tests/train_mnist.sh
+++ pytorch_tests/train_mnist.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/bash
 # call script for tf1 container
 ##SBATCH --gres gpu:1
+#SBATCH --partition sim-normal
 #SBATCH -n 1
 #SBATCH -N 1
 
-container_home=${HOME}/workspace/containers
+
+# cmrr gpu systems
+cmrr_dl_host='gpu4dl'
 
 if [[ $(hostname -s) == "aladdinsane" ]]; then
+	container_home=${HOME}/workspace/containers
 	work_dir=${HOME}/workspace/dltesting/pytorch_tests
 	save_dir=${work_dir}/save
 	files_dir=${work_dir}/files
 	results_dir=${work_dir}/results
+elif [[ $(hostname -s) =~ [^$cmrr_dl_host] ]]; then
+	container_home=${HOME}/containers
+	work_dir=${HOME}/workspace/dltesting/pytorch_tests
+	save_dir=${work_dir}/save
+	files_dir=${work_dir}/files	
+	results_dir=${work_dir}/results
 fi
+
+# create directories, if necessary
+[ -d ${save_dir} ] || mkdir -p ${save_dir}
+[ -d ${files_dir} ] || mkdir -p ${files_dir}
+[ -d ${results_dir} ] || mkdir -p ${results_dir} 
 
 # run containerized pytorch workflow
 singularity exec --nv -B ${save_dir}:/save \
